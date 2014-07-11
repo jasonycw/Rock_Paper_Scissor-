7fa6928684fa2570b4a2f97c82671f24350250d1
Renee <reneetong303@gmail.com>
2014-07-11 16:29:18 +0800

    first comment :P add one servlet and boundground css add draft session to homeServlet

--- Rock_Paper_Scissor/src/cs4280/servlet/ProfileServlet.java
+++ Rock_Paper_Scissor/src/cs4280/servlet/ProfileServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-public class HomeServlet extends HttpServlet {
+public class ProfileServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getRemoteUser();
@@ -17,12 +17,6 @@ public class HomeServlet extends HttpServlet {
         */
 
          HttpSession session = request.getSession();
-         session.setAttribute("username",username);
-         session.setAttribute("isLoginedIn", "1");
-         session.setAttribute("Background","1/2/3");
-         session.setAttribute("currentPage","main");
-           // when this page direct to another page, have to set attribute first.. when page is first loaded, check attribute see if is really this page ==> redirect
-          // set time
 
     }
 

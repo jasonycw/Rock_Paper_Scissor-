f5490050975db34608fcde3c61bb26a4e787db94
Louis. C <tathoic@hotmail.com>
2014-07-20 15:09:06 +0800

    Break in attempt check in ProfileServlet

--- Rock_Paper_Scissor/src/util/SessionValidation.java
+++ Rock_Paper_Scissor/src/util/SessionValidation.java
@@ -1,6 +1,5 @@
 package util;
 
-import cs4280.bean.AckBean;
 import cs4280.bean.PageProgressBean;
 import cs4280.bean.PlayerBean;
 import cs4280.exception.BreakInException;
@@ -8,34 +7,26 @@ import cs4280.exception.BreakInException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
 public class SessionValidation {
 
-    public static void CheckBreakInAttempt(HttpSession session, HttpServletRequest request, HttpServletResponse response) {
+    public static void CheckBreakInAttempt(HttpSession session, HttpServletRequest request, HttpServletResponse response) throws BreakInException {
         PlayerBean player = (PlayerBean) session.getAttribute("playerInfo");
         PageProgressBean pageProgressBean = ((PageProgressBean) session.getAttribute("pageInfo"));
-        try {
-            if (player == null || pageProgressBean == null) {
-                throw new BreakInException();
-            }
 
-            String username = player.getmUsername();
-            String password = player.getmPassword();
-            String sessionKey = SessionTimeConversion.encrypt(player.getmLoginTime());
-            String dbTime = DBCommonUsage.getLoginTime(username, password);
+        if (player == null || pageProgressBean == null) {
+            throw new BreakInException();
+        }
 
-            String encryptedKey = SessionTimeConversion.encrypt(dbTime);
-            if (!encryptedKey.equals(sessionKey)) {
-                throw new BreakInException();
-            }
-        } catch (BreakInException e) {
-            session.setAttribute("ackMsg", new AckBean("Warning", "Break-in attempt"));
-            try {
-                response.sendRedirect(ProjectUrl.getBaseUrl(request) + "/login");
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
+        String username = player.getmUsername();
+        String password = player.getmPassword();
+        String sessionKey = SessionTimeConversion.encrypt(player.getmLoginTime());
+        String dbTime = DBCommonUsage.getLoginTime(username, password);
+
+        String encryptedKey = SessionTimeConversion.encrypt(dbTime);
+        if (!encryptedKey.equals(sessionKey)) {
+            throw new BreakInException();
         }
     }
 }
+
